78a2a11be672519ad4ff1969d3c6948c730672e2
yurimnim <u007x2@gmail.com>
2020-10-11 00:31:16 +0900

    second commit

--- udemy_course_followup/3-how-JS-works/script.js
+++ udemy_course_followup/3-how-JS-works/script.js
@@ -1,6 +1,6 @@
 ///////////////////////////////////////
 // Lecture: Hoisting
-
+/*
  calculateAge(1965);
 
 function calculateAge(year) {
@@ -20,27 +20,19 @@ console.log(age);
 var age = 23;
 console.log(age);
 
-
 function foo() {
     var age = 65; 
     console.log(age);
 }
+
 foo();
 console.log(age);
 
 
-
-
-
-
-
 ///////////////////////////////////////
 // Lecture: Scoping
-
-
 // First scoping example
 
-/*
 var a = 'Hello!';
 first();
 
@@ -53,9 +45,6 @@ function first() {
         console.log(a + b + c);
     }
 }
-*/
-
-
 
 // Example to show the differece between execution stack and scope chain
 
@@ -78,17 +67,38 @@ function third() {
     console.log(a + b + c + d);
 }
 */
-
-
-
 ///////////////////////////////////////
 // Lecture: The this keyword
 
+//console.log(this);
+
+calculateAge(1983);
+function calculateAge(year) {
+    console.log(2020-year);
+    console.log(this);
+}
+
+var john = {
+    name: 'John',
+    yearOfBirth : 1990,
+    calculateAge: function() {
+        console.log(this);
+        console.log(2020 - this.yearOfBirth);
+        function innerFunction() {
+            console.log(this);
+        }
+        innerFunction();
+    }
+}
+
+john.calculateAge();
 
 
+var mike = {
+    name: 'Mike',
+    yearOfBirth: 1984
+};
 
-
-
-
-
+mike.calculateAge = john.calculateAge;
+mike.calculateAge();
 
