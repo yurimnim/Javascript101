abfa1d12563ce24f1339e8adedaddbab7177b044
yurimnim <u007x2@gmail.com>
2021-01-06 00:07:15 +0900

    5th Jan, 2021

--- udemy_course_followup/06-Modal/starter/script.js
+++ udemy_course_followup/06-Modal/starter/script.js
@@ -1,1 +1,29 @@
 'use strict';
+
+const modal = document.querySelector('.modal');
+const overlay = document.querySelector('.overlay');
+const btnCloseModal = document.querySelector('.close-modal');
+const btnOpenModal = document.querySelectorAll('.show-modal');
+
+const openModal = () => {
+  modal.classList.remove('hidden');
+  overlay.classList.remove('hidden');
+};
+
+const hidden = () => {
+  modal.classList.add('hidden');
+  overlay.classList.add('hidden');
+};
+
+for (let i = 0; i < btnOpenModal.length; i++) {
+  btnOpenModal[i].addEventListener('click', openModal);
+}
+
+btnCloseModal.addEventListener('click', hidden);
+overlay.addEventListener('click', hidden);
+
+document.addEventListener('keydown', e => {
+  if (e.key === 'Escape' && !modal.classList.contains('hidden')) {
+    hidden();
+  }
+});
